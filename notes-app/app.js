3d059206965ef0ba853c82c116cf9b81264623c9
jahedev <jahedev@gmail.com>
2021-03-18 17:25:02 -0400

    notesapp

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,8 @@
-getNotes = require('./notes.js')
+const validator = require('validator')
+const chalk = require('chalk')
 
+const getNotes = require('./notes.js')
+const msg = getNotes()
 console.log(getNotes())
+
+console.log(chalk.blue('hello world!', chalk.underline(' hey!')))
