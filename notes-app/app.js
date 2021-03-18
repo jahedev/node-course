b397714123485bcb26fff604b976e303feb5538f
jahedev <jahedev@gmail.com>
2021-03-18 16:54:50 -0400

    command line arguments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,52 @@
-const validator = require('validator')
-const chalk = require('chalk')
+const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
 
-const getNotes = require('./notes.js')
-const msg = getNotes()
-console.log(getNotes())
+yargs.version('1.1.0');
 
-console.log(chalk.blue('hello world!', chalk.underline(' hey!')))
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    console.log('Title:', argv.title, '\n');
+    console.log(argv.body);
+  },
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a new note',
+  handler: function () {
+    console.log('Removing a new note...!');
+  },
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+    console.log('Listing notes...!');
+  },
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+    console.log('Reading note...!');
+  },
+});
+
+yargs.parse();
